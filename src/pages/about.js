a21ec84af6d04e0305bbeffde9eb0acac2ae4970
Garrison McMullen <garrison@Garrisons-MacBook-Pro.local>
2020-04-12 18:09:08 -0400

    Another fix

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,9 +5,14 @@ import Header from '../components/Header'
 function About() {
   return (
     <div>
+      {/* a learned example: use layouts!
+      for example, they bring the best code reuse/styling consistency
+      using html body, for bg color for example, leads to stuff like this.. */}
+      <div class="overlayHack">
+      </div>
       <Header layout="vertical" />
       {/* typewriter art page  */}
-      <div style={{zIndex: -1, marginTop: 12}}>
+      <div style={{zIndex: -1}}>
         <Iframe url="/vanillajs/pixiJSTypeWriter.html" 
             width="100%"
             height="100%"
