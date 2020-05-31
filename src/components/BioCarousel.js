98811246afb8fe298b8dd3e74c81f70e2da7e49e
mcmul <mcmul@LAPTOP-O8MSFBOJ>
2020-05-31 03:53:20 -0400

    Styling changes

--- src/components/BioCarousel.js
+++ src/components/BioCarousel.js
@@ -141,7 +141,7 @@ export default function BioCarousel(props) {
                 <div style={{"maxWidth":"1000"}}>
                   <ItemsCarousel
                     infiniteLoop
-                    numberOfCards={3}
+                    numberOfCards={2}
                     slidesToScroll={1}
                     showSlither={true}
                     activeItemIndex={index}
