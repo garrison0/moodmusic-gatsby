9758b9dce1ce4ca1b4d4ee51f04a7f5d6b31fba8
mcmul <mcmul@LAPTOP-O8MSFBOJ>
2020-05-30 22:52:29 -0400

    Styling changes

--- src/components/BioCarousel.js
+++ src/components/BioCarousel.js
@@ -68,7 +68,7 @@ export default function BioCarousel(props) {
   };
 
   return (
-    <Row style={{background: 'rgb(255,255,255)', height: '100%', minHeight: '35em' overflow: 'hidden'}}>
+    <Row style={{background: 'rgb(255,255,255)', height: '100%', minHeight: '35em', overflow: 'hidden'}}>
       <Row id="about-section" style={{background: 'rgb(255,255,255)', height: '100%', maxWidth: '68em', minHeight: '35em', margin: '0 auto'}} className="border-top border-light"> 
 
         <Col xs={{span: 12, order: 1}} md={{span: 5, order: 0}} className="right_shadow">
