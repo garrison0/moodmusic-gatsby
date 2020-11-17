23056deb92de9f89c74472bc4bc0618809a1d3d4
Garrison McMullen <garrison@Garrisons-MacBook-Pro.local>
2020-11-17 16:28:23 -0500

    Add icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -86,7 +86,7 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         // edit below
-        // icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/geetr.png`,
       },
     },
     {
